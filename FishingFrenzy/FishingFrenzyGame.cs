b8d4ff357eb6ee1c3df0041b42bef985003125e3
koneko-chann <vuhaiha04062002@gmail.com>
2024-11-29 00:15:03 +0700

    Add project files.

---
+++ FishingFrenzy/FishingFrenzyGame.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishingFrenzy
+{
+    public class FishingFrenzyGame
+    {
+        public const int FPS = 20;
+        public const double ADD_FRAM_REPLAY_DELAY = 1f / 20f;
+        public double OneSecondCounter { get; set; }
+        public double AmountEnergy { get; set; }
+        public double AmountMoney { get; set; }
+        public GameComponent Fish { get; set; }=new GameComponent(100);
+        public GameComponent Net { get; set; } = new GameComponent(200);
+        public double EnergyFillRate { get; set; } = 0.21;
+        public double EnergyDrainRate { get; set; } = 0.045;
+        public int CatchFishTime { get; set; } = 20;
+        public int Speed { get; set; } = 150;
+        public List<List<int>> Frs { get; set; } = new List<List<int>>();
+        public FishingFrenzyGame(double energyFillRate,double energyDrainRate)
+        {
+            this.EnergyFillRate = energyFillRate;
+            this.EnergyDrainRate = energyDrainRate;
+        }
+        private bool IsFishInNet()
+        {
+            double fishTop = Fish.PositionY + Fish.Height / 2f;
+            double fishBottom = Fish.PositionY - Fish.Height / 2f;
+            double netTop = Net.PositionY + Net.Height / 2f;
+            double netBottom = Net.PositionY - Net.Height / 2f;
+            return (fishBottom >= netBottom) && (fishTop <= netTop);
+        }
+        private void RandomPosition(MoveDirection moveDirection)
+        {
+            Fish.PositionY=new Random().Next(0,400);
+            Net.PositionY = Fish.PositionY + new Random().Next(-100, 100);
+        }
+        public void AddFrameToReplay(int e = -1, int t = 0)
+        {
+            RandomPosition(MoveDirection.KeepDirection);
+            if (e != -1 && t != 0)
+            {
+                Frs.Add(new List<int>
+            {
+            (int)Math.Round(Fish.PositionY), 
+            (int)Math.Round(Net.PositionY), 
+            e, 
+            t  
+        });
+            }
+            else
+            {
+                Frs.Add(new List<int>
+        {
+            (int)Math.Round(Fish.PositionY),
+            (int)Math.Round(Net.PositionY)
+        });
+            }
+        }
+        public void EmulateFishPosition(MoveDirection moveDirection)
+        {
+
+        }
+        public string Update(double deltaTime)
+        {
+            string jsonMessage = "";
+            OneSecondCounter += ADD_FRAM_REPLAY_DELAY;
+            if (Net != null && Fish != null)
+            {
+                if (IsFishInNet())
+                {
+                    AmountEnergy += EnergyFillRate * deltaTime;
+                    AmountEnergy = Math.Min(1, AmountEnergy); 
+                }
+                else
+                {
+                    AmountEnergy -= EnergyDrainRate * deltaTime;
+                    AmountEnergy = Math.Max(0, AmountEnergy); 
+                }
+
+                if (AmountEnergy >= 1)
+                {
+                    var message = new
+                    {
+                        cmd = "end",
+                        rep = Frs, 
+                        en = AmountEnergy
+                    };
+                    jsonMessage = Newtonsoft.Json.JsonConvert.SerializeObject(message);
+                }
+            }
+
+            if (OneSecondCounter >= ADD_FRAM_REPLAY_DELAY)
+            {
+                AddFrameToReplay(-1, 0);
+                OneSecondCounter = 0;
+            }
+            return jsonMessage;
+        }
+
+    }
+    public class GameComponent
+    {
+        public int Height { get; set; }
+        public double PositionY { get; set; }
+        public int PreviousPositionY { get; set; }
+        public GameComponent(int height)
+        {
+            Height = height;
+        }
+    }
+    public enum MoveDirection
+    {
+        Up=1,
+        KeepDirection=0,
+        Down=-1
+    }
+}
---
+++ FishingFrenzy/Program.cs
@@ -0,0 +1,95 @@
+﻿using FishingFrenzy;
+using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+class WebSocketClient
+{
+    public event Action<string> OnMessageReceived; 
+    private ClientWebSocket webSocket;
+
+    public async Task ConnectAsync(string uri)
+    {
+        webSocket = new ClientWebSocket();
+        Console.WriteLine("Connecting to WebSocket server...");
+        await webSocket.ConnectAsync(new Uri(uri), CancellationToken.None);
+        Console.WriteLine("Connected!");
+
+        _ = Task.Run(() => ReceiveMessagesAsync());
+    }
+
+    public async Task SendMessageAsync(string message)
+    {
+        if (webSocket.State == WebSocketState.Open)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+            await webSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
+            Console.WriteLine($"Message sent: {message}");
+        }
+        else
+        {
+            Console.WriteLine("WebSocket is not open.");
+        }
+    }
+
+    private async Task ReceiveMessagesAsync()
+    {
+        byte[] buffer = new byte[1024];
+
+        while (webSocket.State == WebSocketState.Open)
+        {
+            try
+            {
+                WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
+                    Console.WriteLine("WebSocket connection closed.");
+                }
+                else
+                {
+                    string message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                    Console.WriteLine($"Message received: {message}");
+
+                    // Kích hoạt sự kiện OnMessageReceived
+                    OnMessageReceived?.Invoke(message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in receiving message: {ex.Message}");
+                break;
+            }
+        }
+    }
+
+    public async Task DisconnectAsync()
+    {
+        if (webSocket != null && webSocket.State == WebSocketState.Open)
+        {
+            await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Client Disconnecting", CancellationToken.None);
+            Console.WriteLine("Disconnected from WebSocket server.");
+        }
+    }
+}
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var webSocketClient = new WebSocketClient();
+        webSocketClient.OnMessageReceived += (message) =>
+        {
+            Console.WriteLine($"Received via event: {message}");
+        };
+        await webSocketClient.ConnectAsync("wss://fishing-frenzy-api-0c12a800fbfe.herokuapp.com/?token=");
+        await webSocketClient.SendMessageAsync("{\"cmd\":\"prepare\",\"range\":\"short_range\"}");
+        await Task.Delay(5000);
+        await webSocketClient.SendMessageAsync("{\"cmd\":\"start\"}");
+        await Task.Delay(30000);
+        await webSocketClient.DisconnectAsync();
+        Console.ReadLine();
+    }
+}
